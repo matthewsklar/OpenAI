1d41762ea659cf3a9d7bc2cceff94295af9efa72
matthewsklar227@gmail.com <matthewsklar227@gmail.com>
2017-08-14 22:49:40 -0400

    Added TensorImage

---
+++ Test/TensorImage.py
@@ -0,0 +1,96 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('tmp/data/', one_hot=True)
+
+# Nodes per hidden layer
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+n_classes = 10
+batch_size = 100  # Process 100 images at a time
+
+# 784 = 28 X 28
+x = tf.placeholder('float', [None, 784])  # Input Data
+y = tf.placeholder('float')  # Correct Data
+
+
+def neural_network_model(data):
+    '''
+    Predict the number
+
+    matmul(data, weights) = [1 x n_nodes] = Values of neurons
+    :param data:
+    :return:
+    '''
+
+    hidden_layer_1 = {
+        'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))
+    }
+
+    hidden_layer_2 = {
+        'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))
+    }
+
+    hidden_layer_3 = {
+        'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+        'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))
+    }
+
+    output_layer = {
+        'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+        'biases': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    '''
+    Calculate layers
+    '''
+    # data * weights + biases
+    l1 = tf.add(tf.matmul(data, hidden_layer_1['weights']), hidden_layer_1['biases'])   # input_data * weights + biases
+    l1 = tf.nn.relu(l1)  # Activation Function
+
+    l2 = tf.add(tf.matmul(l1, hidden_layer_2['weights']), hidden_layer_2['biases'])  # layer1 * weights + biases
+    l2 = tf.nn.relu(l2)  # Activation Function
+
+    l3 = tf.add(tf.matmul(l2, hidden_layer_3['weights']), hidden_layer_3['biases'])  # layer2 * weights + biases
+    l3 = tf.nn.relu(l3)  # Activation Function
+
+    output = tf.add(tf.matmul(l3, output_layer['weights']), output_layer['biases'])  # layer3 * weights + biases
+
+    return output
+
+def train_neural_network(data):
+    prediction = neural_network_model(data)
+
+    # Error between prediction and actual output
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+
+    # Use Adam Optimizer to minimize cost
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    n_epochs = 10  # Cycles of (feed forward + back prop)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(n_epochs):
+            epoch_loss = 0
+
+            for _ in range(int(mnist.train.num_examples / batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+
+                epoch_loss += c
+
+            print('Epoch %d completed out of %d loss: %f' % (epoch + 1, n_epochs, epoch_loss))
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))  # output = expected output
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+        print('Accuracy: %f' % (accuracy.eval({x: mnist.test.images, y: mnist.test.labels})))
+
+train_neural_network(x)

be49c9c5f87323101924000726425030db8b99e7
matthewsklar227@gmail.com <matthewsklar227@gmail.com>
2017-08-18 14:09:37 -0400

    Created second convolutional network

---
+++ Other/ConvolutionalNetworkV2/main.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+from tensorflow.contrib import learn
+from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def cnn_model_fn():
+    pass
